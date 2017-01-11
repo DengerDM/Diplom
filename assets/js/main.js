b70e1522ca6863be77e2c893e1258d40ee52fada
DengerDM <denger@rambler.ru>
2017-01-11 20:37:35 +0200

    first step

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  console.log("2");
+  $('.slider_2').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots:true,
+  autoplay:true,
+  speed:300
+});
+});
